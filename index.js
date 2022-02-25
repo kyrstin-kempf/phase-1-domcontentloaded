57211dbe235534cddbfbc3c1970a6b71daf27459
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-02-25 14:34:18 -0500

    commit tree 036bc920d387086c7f8fcac15b272b304f109b74

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+console.log(
+    "Before DOM Loads");
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("After DOM Loads");
+    console.log(document.querySelector('#text').textContent = "This is really cool!");
+  });
+
+// document.addEventListener("DOMContentLoaded", function() {
+// document.querySelector('#text').replaceWith("This is really cool!");
